9688f2f8ed67e14c26ab391fcc7ac8540237ea7a
TaeWoongYoun <24457545yong@gmail.com>
2025-09-24 12:20:43 +0900

    feat : Update models

--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Question(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_data = models.DateTimeField()
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.TextField()
+    create_data = models.DateTimeField()
+
+
 # Create your models here.
