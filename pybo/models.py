846d9f71e9c5c52068297dcce9e1f4ad27000117
TaeWoongYoun <24457545yong@gmail.com>
2025-10-09 00:33:14 +0900

    feat : Create admin

--- pybo/models.py
+++ pybo/models.py
@@ -3,12 +3,14 @@ from django.db import models
 class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
-    create_data = models.DateTimeField()
+    create_date = models.DateTimeField()
+    def __str__(self):
+        return self.subject
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    answer = models.TextField()
-    create_data = models.DateTimeField()
+    content = models.TextField()
+    create_date = models.DateTimeField()
 
 
 # Create your models here.
