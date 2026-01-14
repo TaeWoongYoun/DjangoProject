b11031e58c7278c6e3cd90d2e434d407de63cb88
TaeWoongYoun <24457545yong@gmail.com>
2026-01-14 14:00:51 +0900

    refector : Debug

--- common/forms.py
+++ common/forms.py
@@ -2,9 +2,9 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class UserFrom(UserCreationForm):
+class UserForm(UserCreationForm):
     email = forms.EmailField(label='이메일')
 
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2', 'email')
+        fields = ('username', 'email')
